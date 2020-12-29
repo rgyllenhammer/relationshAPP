410be0c587501fca2cb94ab205a34cc18d0a3a49
Reese Gyllenhammer <reesegyllenhammer@Reeses-MacBook-Pro-2.local>
2020-12-29 15:16:27 -0800

    styled the search bar better

--- anissagram/anissagram/Views/TestView.swift
+++ anissagram/anissagram/Views/TestView.swift
@@ -9,22 +9,38 @@ import SwiftUI
 
 struct TestView: View {
     @State var userTerm = ""
+    @State var showingUseers = false
     var names = ["reese", "ranchgod", "anissa", "jadyn", "jaqueline", "reese2"]
 
     var body: some View {
-
         VStack{
-            SearchBar(userTerm: $userTerm, names: names)
+            SearchBar(userTerm: $userTerm, showingUsers: $showingUseers)
+            ZStack {
+                ScrollView {
+                    HStack{
+                        Text("Happy Birthday Anissa!").font(.largeTitle).fontWeight(.bold)
+                        Spacer()
+                    }.padding(.top)
+                    HStack {
+                        Text("You are probably wondering what this app does, so I will break it down by each tab.")
+                        Spacer()
+                    }.padding(.bottom).foregroundColor(.gray)
+                }
+                SearchResults(userTerm: $userTerm, showingUsers: $showingUseers, names: names)
+            }
             Spacer()
-        }
+        }.padding(.horizontal)
     }
 }
 
 struct SearchBar : View {
 
     @Binding var userTerm : String
-    @State var showingUsers = false
-    var names : [String]
+    @Binding var showingUsers : Bool
+    
+    func hideKeyBoard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
 
 
     var body: some View {
@@ -32,44 +48,57 @@ struct SearchBar : View {
             HStack {
                 TextField("Search users ...", text: $userTerm)
                     .padding(.all, 10)
-                    .font(.title2)
+                    .font(.title3)
                     .background(Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0))
                     .cornerRadius(5)
-                    .padding()
+//                    .padding()
                 if (self.showingUsers) {
                     Button(action: {
                         self.showingUsers = false
                         self.userTerm = ""
+                        self.hideKeyBoard()
                     }, label: {
-                        Text("Clear").font(.title3)
+                        Text("Cancel").font(.title3)
                     })
                     .padding(.trailing)
                 }
             }.onTapGesture {
                 self.showingUsers = true
             }
-            if (self.showingUsers && !self.userTerm.isEmpty) {
-                ScrollView{
-                    ForEach((names).filter({$0.contains(userTerm.lowercased())}), id: \.self) { name in
-                        HStack {
-                            Image("anissagram").resizable()
-                                .frame(width: 55, height: 55)
-                                .clipShape(Circle())
-                            VStack(alignment: .leading) {
-                                Text("@\(name)").fontWeight(.semibold)
-                                Text("Choose Relationship")
-                                Divider()
-                            }
-                            .padding(.top)
-
-                        }.padding(.horizontal)
-                    }
-            }
-            }
-
         }
     }
 }
+
+struct SearchResults : View {
+
+    @Binding var userTerm : String
+    @Binding var showingUsers : Bool
+    var names : [String]
+
+
+    var body: some View {
+        if (self.showingUsers && !self.userTerm.isEmpty) {
+            ScrollView{
+                ForEach((names).filter({$0.contains(userTerm.lowercased())}), id: \.self) { name in
+                    HStack {
+                        Image("anissagram").resizable()
+                            .frame(width: 55, height: 55)
+                            .clipShape(Circle())
+                        VStack(alignment: .leading) {
+                            Text("@\(name)").fontWeight(.semibold)
+                            Text("Choose Relationship")
+                            Divider()
+                        }
+                        .padding(.top)
+
+                    }
+//                    .padding(.horizontal)
+                }
+            }.background(Color.white)
+        }
+    }
+}
+
 
 
 struct TestView_Previews: PreviewProvider {
